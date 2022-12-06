812be18066cc959739f8efb2f960f59cfd976b8a
Ariana <73635827+arianadaris@users.noreply.github.com>
2022-12-06 09:37:11 -0700

    Update section dimensions

--- pages/index.js
+++ pages/index.js
@@ -9,16 +9,16 @@ export default function Home() {
       <Head>
         <title>Ariana Rajewski | ASU Graduation</title>
         <meta name="description" content="Ariana Rajewski ASU Graduation Announcement" />
-        <link rel="icon" type="image/x-icon" href="/images/favicon.png" />
+        <link rel="icon" href="/images/favicon.png" />
       </Head>
 
       <main className="">
         {/* Home Section */}
         <div className="lg:flex lg:flex-row xs:flex-col xs:h-full md:h-screen">
-          <div className="lg:w-[45%] xs:w-full lg:h-[100vh] xs:h-[35vh]">
+          <div className="lg:w-[45%] xs:w-full lg:h-[100vh] xs:h-[40vh]">
             <PhotoCarousel className="h-full" />
           </div>
-          <div className="lg:w-[55%] xs:w-full bg-[#F5F3F0] flex flex-col lg:justify-center items-center px-[10vw] lg:h-full xs:h-[55vh] lg:pt-0 md:pt-12 xs:pt-6">
+          <div className="lg:w-[55%] xs:w-full bg-[#F5F3F0] flex flex-col justify-center items-center px-[10vw] lg:h-full xs:h-[60vh] lg:pt-0 md:pt-12 xs:pt-2">
             <Image className="asuLogo" src={require('../public/images/ASU_Logo.png')} />
             <h2>Class of 2022</h2>
             <h1>Ariana Rajewski</h1>
@@ -38,9 +38,9 @@ export default function Home() {
 
         {/* Thank You Section */}
         <a id="thankyou"><div className="h-screen bg-[#F1EFEB] flex relative">
-          <div className="lg:w-[60%] xs:w-full lg:px-20 xs:px-10 lg:py-[25vh] md:py-[10vh] xs:py-10">
+          <div className="lg:w-[60%] xs:w-full lg:px-20 xs:px-10 lg:py-[20vh] md:py-[10vh] xs:py-10">
             <div className="lg:flex lg:flex-row xs:flex-col items-center">
-              <h1 className="lg:w-[50%] xs:text-center lg:text-left">THANK YOU</h1>
+              <h1 className="md:w-[70%] xs:text-center lg:text-left">THANK YOU</h1>
               <div className="border-b-[1px] border-black lg:w-[100%] xs:w-[50%] mb-8 mt-6 mx-auto" />
             </div>
             <div className="lg:my-20">
@@ -52,12 +52,12 @@ export default function Home() {
               </h3>
               <div className="flex xs:flex-col lg:flex-row lg:justify-between lg:items-center">
                 <h3 className="xs:text-center lg:text-left">Thank you all for being a part of my journey.</h3>
-                <h5 className="lg:mt-0 xs:mt-6 xs:text-center lg:text-left"><span className="font-ogg">-</span> Ariana Rajewski</h5>
+                <h5 className="lg:mt-0 xs:mt-6 xs:text-center lg:text-left w-[50%]"><span className="font-ogg">-</span> Ariana Rajewski</h5>
               </div>
             </div>
             <a href="https://www.arianadaris.dev/" target="_blank" rel="noreferrer"><h4 className="absolute bottom-5 hover:text-[rgba(0,0,0,0.75)]">arianadaris.dev</h4></a>
           </div>
-          <div className="lg:w-[40%] xs:w-0">
+          <div className="lg:w-[40%] xs:w-0 md:mt-[-2vh]">
             <Image src={require('../public/images/Ari_2.jpg')} className="personalImage" />
           </div>
         </div></a>
